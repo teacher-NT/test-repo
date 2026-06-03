738dbb7742978e02b0a1786029915204c944f412
Teacher NT <holmonbek@gmail.com>
2026-06-03 14:41:41 +0500

    new

--- 1.py
+++ 1.py
@@ -1,13 +1,4 @@
-#import math
-#import requests
-#import random
-#import datetime
-#import os, sys
-#import time
-#import pytz
-#import json
-# from deep_translator import GoogleTranslator as tarjimon
-#from collections import Counter, defaultdict
+
 print('\033[2J\033[3J\033[H', end='')
 import requests
 
